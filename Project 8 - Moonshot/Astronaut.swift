7d0869acbd64e34825907965c2d98d578e4c80b5
Luca M <36612417+electroqobra@users.noreply.github.com>
2021-01-04 22:13:21 -0500

    Add files via upload

---
+++ Project 8 - Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Luca M on 1/4/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
